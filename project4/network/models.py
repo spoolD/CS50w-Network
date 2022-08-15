b5026ad7589b8f6cfb9400c298e8a0a34a75c672
Davy Sproule <dsproule@vt.edu>
2022-08-15 07:54:43 -0400

    Add posting model

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,6 +1,14 @@
+from tkinter import CASCADE
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+class Posting(models.Model):
+    #relationship to User, timestamp, body, users who like
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    liked = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(blank=True)
