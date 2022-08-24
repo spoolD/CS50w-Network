50a039185df8666eec7d4c9219f12e8f2df729b7
Davy Sproule <dsproule@vt.edu>
2022-08-23 20:13:14 -0400

    Made a mess of models

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,10 +4,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    #following
-
-    #followed by
-    
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers', blank=True, null=True)
     def __str__(self):
         return self.username
 
@@ -17,3 +14,4 @@ class Posting(models.Model):
     liked = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='likes')
     timestamp = models.DateTimeField(auto_now_add=True)
     body = models.TextField(blank=True)
+
